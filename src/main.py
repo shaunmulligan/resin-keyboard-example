237272f09b89ee2063164bfd3b901aa1970ffd09
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-05-09 18:55:51 +0100

    simplify example and remove gpio dep

--- src/main.py
+++ src/main.py
@@ -1,61 +1,21 @@
 #!/usr/bin/env python
 
-# gpio_status.py
-# 2015-03-04
-# Public Domain
-
-import time
 import curses
-import atexit
-
-import pigpio
-
-GPIOS=32
-
-MODES=["INPUT", "OUTPUT", "ALT5", "ALT4", "ALT0", "ALT1", "ALT2", "ALT3"]
-
-def cleanup():
-    curses.nocbreak()
-    curses.echo()
-    curses.endwin()
-    pi.stop()
-
-pi = pigpio.pi()
-
 stdscr = curses.initscr()
-curses.noecho()
 curses.cbreak()
+stdscr.keypad(1)
 
-atexit.register(cleanup)
+stdscr.addstr(0,10,"Hit 'q' to quit")
+stdscr.refresh()
 
-cb = []
-
-for g in range(GPIOS):
-    cb.append(pi.callback(g, pigpio.EITHER_EDGE))
-
-stdscr.nodelay(1)
-
-stdscr.addstr(0, 23, "Status of gpios 0-31", curses.A_REVERSE)
-
-while True:
-
-    for g in range(GPIOS):
-        tally = cb[g].tally()
-        mode = pi.get_mode(g)
-
-        col = (g / 11) * 25
-        row = (g % 11) + 2
-
-        stdscr.addstr(row, col, "{:2}".format(g), curses.A_BOLD)
-
-        stdscr.addstr(
-         "={} {:>6}: {:<10}".format(pi.read(g), MODES[mode], tally))
-
+key = ''
+while key != ord('q'):
+    key = stdscr.getch()
+    stdscr.addch(20,25,key)
     stdscr.refresh()
+    if key == curses.KEY_UP:
+        stdscr.addstr(2, 20, "Up")
+    elif key == curses.KEY_DOWN:
+        stdscr.addstr(3, 20, "Down")
 
-    time.sleep(0.1)
-
-    c = stdscr.getch()
-
-    if c != curses.ERR:
-        break
+curses.endwin()
