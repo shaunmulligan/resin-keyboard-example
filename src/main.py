d730ca5b0a303e66f404b5fa4d40647d194c9ca5
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-05-09 19:54:07 +0100

    use a different example

--- src/main.py
+++ src/main.py
@@ -1,22 +1,35 @@
 #!/usr/bin/env python
+import sys
 
-import curses
-stdscr = curses.initscr()
-curses.cbreak()
-stdscr.keypad(1)
+try:
+    import tty, termios
+except ImportError:
+    # Probably Windows.
+    try:
+        import msvcrt
+    except ImportError:
+        # FIXME what to do on other platforms?
+        # Just give up here.
+        raise ImportError('getch not available')
+    else:
+        getch = msvcrt.getch
+else:
+    def getch():
+        """getch() -> key character
 
-stdscr.addstr(0,10,"Hit 'q' to quit")
-stdscr.refresh()
+        Read a single keypress from stdin and return the resulting character.
+        Nothing is echoed to the console. This call will block if a keypress
+        is not already available, but will not wait for Enter to be pressed.
 
-key = ''
-while key != ord('q'):
-	print 'I am in a loop!'
-    key = stdscr.getch()
-    stdscr.addch(20,25,key)
-    stdscr.refresh()
-    if key == curses.KEY_UP:
-        stdscr.addstr(2, 20, "Up")
-    elif key == curses.KEY_DOWN:
-        stdscr.addstr(3, 20, "Down")
-
-curses.endwin()
+        If the pressed key was a modifier key, nothing will be detected; if
+        it were a special function key, it may return the first character of
+        of an escape sequence, leaving additional characters in the buffer.
+        """
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
